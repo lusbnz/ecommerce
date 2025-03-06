cf95661c252de49e96cc711265efe497fd84c1c1
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2025-03-06 14:26:02 +0700

    update

--- src/data.js
+++ src/data.js
@@ -1,76 +1,76 @@
 export const sliderItems = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/20475584/pexels-photo-20475584/free-photo-of-k-t-n-i-may-vi-tinh-chia-khoa-b-c-th.jpeg",
-      title: "SUMMER SALE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "f5fafd",
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/17038226/pexels-photo-17038226/free-photo-of-ban-phim-ngh-nhan-qu-tuy-ch-nh.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      title: "AUTUMN COLLECTION",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fcf1ed",
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/6623764/pexels-photo-6623764.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      title: "LOUNGEWEAR LOVE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fbf0f4",
-    },
-  ];
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/20475584/pexels-photo-20475584/free-photo-of-k-t-n-i-may-vi-tinh-chia-khoa-b-c-th.jpeg",
+    title: "KEYCAP SALE",
+    desc: "UPGRADE YOUR KEYBOARD! GET 30% OFF ON NEW KEYCAP SETS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/17038226/pexels-photo-17038226/free-photo-of-ban-phim-ngh-nhan-qu-tuy-ch-nh.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "PREMIUM MECHANICAL KEYBOARDS",
+    desc: "EXPERIENCE THE BEST TYPING FEEL! GET 30% OFF ON TOP-SELLING KEYBOARDS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/6623764/pexels-photo-6623764.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "HIGH-PERFORMANCE GAMING MOUSE",
+    desc: "DON'T MISS OUT! GET 30% OFF ON PREMIUM GAMING MICE TODAY.",
+    bg: "fbf0f4",
+  },
+];
 
-  export const categories = [
-    {
-      id: 1,
-      img: "https://www.lofree.co/cdn/shop/files/Flow84_Marble_PI01.webp?v=1727494132&width=1600",
-      title: "Keyboards",
-    },
-    {
-      id: 2,
-      img: "https://www.lofree.co/cdn/shop/files/Matrix84PI01_069562a4-d828-4aca-901e-fd2af7f954d6.webp?v=1727583061&width=1600",
-      title: "Keycaps",
-    },
-    {
-      id: 3,
-      img: "https://www.lofree.co/cdn/shop/files/FoundationNumpad_PI01_f02d2b43-37d7-45dc-afa3-3ac8aa958eba.webp?v=1719305159&width=1400",
-      title: "Accessories",
-    },
-  ];
+export const categories = [
+  {
+    id: 1,
+    img: "https://www.lofree.co/cdn/shop/files/Flow84_Marble_PI01.webp?v=1727494132&width=1600",
+    title: "Keyboards",
+  },
+  {
+    id: 2,
+    img: "https://www.lofree.co/cdn/shop/files/Matrix84PI01_069562a4-d828-4aca-901e-fd2af7f954d6.webp?v=1727583061&width=1600",
+    title: "Keycaps",
+  },
+  {
+    id: 3,
+    img: "https://www.lofree.co/cdn/shop/files/FoundationNumpad_PI01_f02d2b43-37d7-45dc-afa3-3ac8aa958eba.webp?v=1719305159&width=1400",
+    title: "Accessories",
+  },
+];
 
-  export const popularProducts = [
-    {
-      id:1,
-      img:"https://www.lofree.co/cdn/shop/files/Candylamp_white_PI01.png?v=1714121967&width=1400",
-    },
-    {
-      id:2,
-      img:"https://www.lofree.co/cdn/shop/files/1_MoruPI01.webp?v=1713926589&width=1400",
-    },
-    {
-      id:3,
-      img:"https://www.lofree.co/cdn/shop/files/DeskMatTofuPI01_6434e0d7-e43f-488e-ac0e-3f5365607c75.webp?v=1736765857&width=1400",
-    },
-    {
-      id:4,
-      img:"https://www.lofree.co/cdn/shop/files/Vangoph84PI01.webp?v=1730433408&width=1600",
-    },
-    {
-      id:5,
-      img:"https://www.lofree.co/cdn/shop/files/Lune68PI01.webp?v=1718766225&width=1600",
-    },
-    {
-      id:6,
-      img:"https://www.lofree.co/cdn/shop/files/DearKittyFlow84PI01.webp?v=1730437774&width=1600",
-    },
-    {
-      id:7,
-      img:"https://www.lofree.co/cdn/shop/files/Specter_SwitchesPI01.webp?v=1727406754&width=1400",
-    },
-    {
-      id:8,
-      img:"https://www.lofree.co/cdn/shop/files/TerrazzoChipsmouse_PI01.webp?v=1719281657&width=1600",
-    },
-  ]
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://www.lofree.co/cdn/shop/files/Candylamp_white_PI01.png?v=1714121967&width=1400",
+  },
+  {
+    id: 2,
+    img: "https://www.lofree.co/cdn/shop/files/1_MoruPI01.webp?v=1713926589&width=1400",
+  },
+  {
+    id: 3,
+    img: "https://www.lofree.co/cdn/shop/files/DeskMatTofuPI01_6434e0d7-e43f-488e-ac0e-3f5365607c75.webp?v=1736765857&width=1400",
+  },
+  {
+    id: 4,
+    img: "https://www.lofree.co/cdn/shop/files/Vangoph84PI01.webp?v=1730433408&width=1600",
+  },
+  {
+    id: 5,
+    img: "https://www.lofree.co/cdn/shop/files/Lune68PI01.webp?v=1718766225&width=1600",
+  },
+  {
+    id: 6,
+    img: "https://www.lofree.co/cdn/shop/files/DearKittyFlow84PI01.webp?v=1730437774&width=1600",
+  },
+  {
+    id: 7,
+    img: "https://www.lofree.co/cdn/shop/files/Specter_SwitchesPI01.webp?v=1727406754&width=1400",
+  },
+  {
+    id: 8,
+    img: "https://www.lofree.co/cdn/shop/files/TerrazzoChipsmouse_PI01.webp?v=1719281657&width=1600",
+  },
+];
